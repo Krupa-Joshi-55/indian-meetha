4b98b465b2b8dadef2db4196d6889cc28cc3a652
Krupa Joshi <krupajoshi55@gmail.com>
2024-04-23 18:07:11 +0530

    Search update

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 document.getElementById('searchForm').addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevent default form submission behavior
-
-  // Perform search functionality here
-  let searchQuery = document.getElementById('search').value.toLowerCase();
-  let cards = document.querySelectorAll('.content .card');
-  
-  cards.forEach(function(card) {
-      let title = card.querySelector('.card-title').innerText.toLowerCase();
-      if (title.includes(searchQuery)) {
-          card.style.display = 'block';
-      } else {
-          card.style.display = 'none';
-      }
+    event.preventDefault(); // Prevent default form submission behavior
+    performSearch();
   });
-});
-
-//  chikki, imarti, modak, kalakand, kaju katli, kheer, phirni, kulfi, mysore pak, petha, rabri, rasmalia, rajbhog, rasgulla, sandes, sheer khurma, shrikhand, suji halwa, til ladoo, malpua, anarsa, achappam, 
+  
+  document.getElementById('search').addEventListener('input', function() {
+    performSearch();
+  });
+  
+  function performSearch() {
+    let searchQuery = document.getElementById('search').value.trim().toLowerCase();
+    let cards = document.querySelectorAll('.content .card');
+    
+    cards.forEach(function(card) {
+        let title = card.querySelector('.card-title').innerText.toLowerCase();
+        if (title.includes(searchQuery) || searchQuery === '') {
+            card.style.display = 'block';
+        } else {
+            card.style.display = 'none';
+        }
+    });
+  }
+  
